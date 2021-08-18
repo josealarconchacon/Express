49e73e74c49f372e9def27da38a3fdaa0acaf138
Jose Carlos <josealarconchacon@gmail.com>
2021-08-18 12:08:48 -0400

    d

---
+++ Express_Server/index.js
@@ -0,0 +1,12 @@
+const express = require("express"); // require express
+const app = express(); 
+
+
+// run app.use every time there is an incoming request
+app.use(() => { 
+    console.log("New Request");
+})
+
+app.listen(3000, () => {
+    console.log("listen on port 3000");
+})
