f5c948c8eca29efdeca6643772059a1883f50721
Jose Carlos <josealarconchacon@gmail.com>
2021-08-22 22:18:15 -0400

    conneting mongoose to mongo

---
+++ Connecting_Mongo_Mongoose/Mongoose_Intro/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+// get mongoose to connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/carCheck', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+    console.log("Connection open............");
+    })
+    .catch(error => {
+    console.log("Something went wrong. :(", error);
+    })
