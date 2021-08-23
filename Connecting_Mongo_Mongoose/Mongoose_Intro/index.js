68cabae97eb1a7bad48d9a2cbad32ed03d0c1dba
Jose Carlos <josealarconchacon@gmail.com>
2021-08-22 22:30:24 -0400

    defining schema

--- Connecting_Mongo_Mongoose/Mongoose_Intro/index.js
+++ Connecting_Mongo_Mongoose/Mongoose_Intro/index.js
@@ -7,3 +7,17 @@ mongoose.connect('mongodb://localhost:27017/carCheck', {useNewUrlParser: true, u
     .catch(error => {
     console.log("Something went wrong. :(", error);
     })
+
+    // defining schema
+    const car_schema = new mongoose.Schema ({
+        make: String,
+        model: String,
+        color: String,
+        year: Number,
+        price: Number,
+        mile: Number
+    })
+
+    // make model using car_schema
+    const Car = mongoose.model('Car', car_schema);
+    
