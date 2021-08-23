e2b56097ec0c6f709bb405335a0ffc3fb87b5e2c
Jose Carlos <josealarconchacon@gmail.com>
2021-08-23 17:48:35 -0400

    Mongoose Virtuals

---
+++ Connecting_Mongo_Mongoose/Mongoose_Intro/person.js
@@ -0,0 +1,21 @@
+const { kMaxLength } = require('buffer');
+const mongoose = require('mongoose');
+// get mongoose to connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/person', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("Connection open............");
+    })
+    .catch(error => {
+        console.log("Something went wrong. :(", error);
+    })
+
+    const personSchema = new mongoose.Schema({
+        first: String,
+        last: String
+    })
+
+    personSchema.virtual('fullName').get(function() {
+        return `${this.first} ${this.last}`
+    })
+
+    const Person = mongoose.model('Person', personSchema);
