8637d8f12be389e043507110eda4e8a45a851b6c
Jose Carlos <josealarconchacon@gmail.com>
2021-08-23 19:31:04 -0400

    add route for detail page

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product');
+
 // integrate mongoose
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('Mongoose: Connection Open.....');
     })
@@ -16,8 +18,20 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUn
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('Woof')
+// route
+app.get('/products', async (req, res) => {
+    // get all products
+    const products = await Product.find({})
+    console.log(products);
+    res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    // find product by id
+    const foundProductById =  await Product.findById(id)
+    // console.log(foundProductById)
+    res.render('products/detail', { foundProductById })
 })
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
