02fd7c408af10f85385e80107307a157cb47b794
Jose Carlos <josealarconchacon@gmail.com>
2021-08-24 14:23:41 -0400

    add form route

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 
 // route
 app.get('/products', async (req, res) => {
@@ -24,6 +25,18 @@ app.get('/products', async (req, res) => {
     const products = await Product.find({})
     console.log(products);
     res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+// form to submit product
+app.post('/products', async (req, res) => {
+    // Make a new product
+    const addNewProduct = new Product(req.body)
+    await addNewProduct.save()
+    console.log(addNewProduct)
+    res.redirect(`/products/${addNewProduct._id}`)
 })
 
 app.get('/products/:id', async (req, res) => {
