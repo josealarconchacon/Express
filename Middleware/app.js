42127282b0eda3cb08a18b9fa148470ee20ae495
Jose Carlos <josealarconchacon@gmail.com>
2021-08-24 23:11:35 -0400

    Define error-handling

--- Middleware/app.js
+++ Middleware/app.js
@@ -6,22 +6,30 @@ const app = express();
 // tell the express to use morgan
 app.use(morgan('tiny'))
 
-app.use((req, res, next) => { // defining Middleware
-    console.log('First mIDDLEWARE');
-    return next(); // callBack
-});
+// app.use((req, res, next) => { // defining Middleware
+//     console.log('First mIDDLEWARE');
+//     return next(); // callBack
+// });
+
 app.use((req, res, next) => {
-    console.log('Second mIDDLEWARE');
-    return next(); // callBack
-});
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next()
+})
+
 
 app.get('/', (req, res) => {
     res.send('Welcome To The Home Page')
 });
 
 app.get('/cars', (req, res) => {
+    console.log(`Request date: ${req.requestTime}`)
     res.send('Vroom Vroom Vroom')
 });
+
+app.use((req, res) => { // Define error-handling 
+    res.status(404).send('Something broke!')
+})
 
 
 app.listen(3000, () => {
